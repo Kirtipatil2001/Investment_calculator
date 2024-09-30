ec318f4ea5d40785d688921a5333cac08772f8bf
Kirti Ravindra Patil <79988064+Kirtipatil2001@users.noreply.github.com>
2024-09-30 16:04:34 +0530

    Add files via upload

---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,15 @@
+ export interface InvestmentInput
+ {
+    initialInvestment: number,
+    duration: number,
+     annualInvestment : number,
+     expectedReturn : number
+ }
+
+// type InvesmentInput =
+// {
+//    initialInvestment: number,
+//     duration: number,
+//     annualInvestment : number,
+//     expectedReturn : number
+// }
